6edcb3119409a2ed3cce77b1e30fac681a4da73e
prateekch33 <prateekchandra27@outlook.com>
2022-08-11 22:54:21 +0530

    Update Server.js

--- server.js
+++ server.js
@@ -1,49 +1,46 @@
-const express = require('express');
-const bodyParser = require('body-parser')
-const connect = require('./db')
-const port = process.env.PORT; 
-require('dotenv').config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const connect = require("./db");
+require("dotenv").config();
 const app = express();
 connect();
 
-app.set('view engine','ejs');
+app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(express.static(__dirname + '/'));
+app.use(express.static(__dirname + "/"));
 
-app.use('/question',require('./route/question'));
-app.use('/user',require('./route/user'));
-app.use('/admin',require('./route/admin'));
-app.use('/admin/result',require('./route/result'));
-app.use('/time',require('./route/timing'));
+app.use("/question", require("./route/question"));
+app.use("/user", require("./route/user"));
+app.use("/admin", require("./route/admin"));
+app.use("/admin/result", require("./route/result"));
+app.use("/time", require("./route/timing"));
 
-app.get('/quiz',(req,res)=>{
-    res.render('quiz');
-})
+app.get("/quiz", (req, res) => {
+  res.render("quiz");
+});
 
-app.get('/data',(req,res)=>{
-    res.render('userdetail');
-})
+app.get("/data", (req, res) => {
+  res.render("userdetail");
+});
 
-app.get('/addquiz',(req,res)=>{
-    res.render('question.ejs');
-})
+app.get("/addquiz", (req, res) => {
+  res.render("question.ejs");
+});
 
-app.get('/instruction',(req,res)=>{
-    res.render('instruction');
-})
+app.get("/instruction", (req, res) => {
+  res.render("instruction");
+});
 
-app.get('/admin',(req,res)=>{
-    res.render('admin');
-})
+app.get("/admin", (req, res) => {
+  res.render("admin");
+});
 
-app.get('/',(req,res)=>{
-    res.render('index');
-})
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
-
-
-app.listen(port,process.env.IP,()=>{
-    console.log('server start at '+port);
-})
+app.listen(process.env.PORT, process.env.IP, () => {
+  console.log("server start at " + process.env.PORT);
+});
