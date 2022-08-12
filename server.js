37c0b9534a1dce3117f0ef2c8ec770734ecd829a
harshrastogi15 <hrastogi2002@gmail.com>
2022-08-12 16:51:20 +0530

    update

--- server.js
+++ server.js
@@ -41,6 +41,6 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
-app.listen(process.env.PORT, process.env.IP, () => {
+app.listen(process.env.PORT|2000, process.env.IP, () => {
   console.log("server start at " + process.env.PORT);
 });
