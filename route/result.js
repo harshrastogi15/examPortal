990e61469e21ec91bde94aba84e60c52e28619dd
harshrastogi15 <hrastogi2002@gmail.com>
2022-12-15 12:31:35 +0530

    udpated

--- route/result.js
+++ route/result.js
@@ -10,11 +10,13 @@ router.get('/Generateresult', async (req, res) => {
 
     try {
         let user = await User.find({});
+        // console.log(user)
         for (i in user) {
-            if(user.answer==undefined){
+            if(user[i].answer==undefined){
                 continue;
             }
             var marks = 0;
+            // console.log(user[i].stream)
             if (user[i].stream === 'Assistant Professor (Level-10) in CSE Department') {
                 for (var j = 0; j < user[i].answer.length; j++) {
                     var q = await CSE.findById(user[i].answer[j].key);
@@ -32,7 +34,8 @@ router.get('/Generateresult', async (req, res) => {
                         marks++;
                     }
                 }
-            }else if (user[i].stream === 'Assistant Professor (Level-10) in MEA Department') {
+            }else if (user[i].stream == "Assistant Professor (Level-10) in MEA Department") {
+                // console.log("HERE")
                 for (var j = 0; j < user[i].answer.length; j++) {
                     var q = await MEA.findById(user[i].answer[j].key);
                     if (user[i].answer[j].value === q.answer) {
