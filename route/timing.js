71b8545aa77ba057253acd07faf7bcd3dea9857b
harshrastogi15 <hrastogi2002@gmail.com>
2022-12-18 20:19:43 +0530

    sort

--- route/timing.js
+++ route/timing.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 router.get('/timing',(req,res)=>{
     const SDate = 'December 18, 2022, 13:18:00';
-    const EDate = 'December 18, 2022, 23:50:00';
+    const EDate = 'December 18, 2022, 20:19:00';
     const presentDate = new Date();
     // console.log(presentDate)
     res.json({SDate:SDate, EDate:EDate, presentDate : presentDate});
